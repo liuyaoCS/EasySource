616a05134100b3f33eab40b5f288827bc18d8d60
liuyaoCS <liuyao@chinaso.com>
2016-08-26 19:50:29 +0800

    handler机制说明

--- app/src/main/java/com/ly/easysource/handler/HandlerTestActivity.java
+++ app/src/main/java/com/ly/easysource/handler/HandlerTestActivity.java
@@ -30,19 +30,20 @@ public class HandlerTestActivity extends Activity {
             public void onClick(View v) {
                 Handler h=t.getHandler();
 
+                //1 空消息
                 h.sendEmptyMessage(1);
-
+                //2 普通消息
                 Message msg=new Message();
                 msg.what=2;
                 h.sendMessage(msg);
-
+                //3 runnable消息 msg.callback=runnable
                 h.post(new Runnable() {
                     @Override
                     public void run() {
                         Log.i("ly","runnable msg");
                     }
                 });
-
+                //4 显示构造runnable消息
                 Message msg2= Message.obtain(h, new Runnable() {
                     @Override
                     public void run() {
@@ -51,7 +52,7 @@ public class HandlerTestActivity extends Activity {
                 });
                 msg2.what=4;
                 msg2.sendToTarget();
-
+                //5 发送子线程退出信号，否则子线程一直处于loop状态
                 h.sendEmptyMessage(5);
             }
         });
--- app/src/main/java/com/ly/easysource/handler/MyThread.java
+++ app/src/main/java/com/ly/easysource/handler/MyThread.java
@@ -16,6 +16,18 @@ public class MyThread extends Thread {
 
     }
     @Override
+    /**
+     * run函数才是处于异步线程中，如果把mHandler的构造放到MyThread的构造函数中，则handler依然出于主线程中
+     * 流程：
+     * 1 Looper.prepare()创建Looper（Looper构造函数创建MessageQueue），存储在ThreadLocal中。
+     * 2 子线程中创建Handler，注意，1,2步骤可以交换。
+     * 3 Looper.loop();MessageQueue中不停的取消息，并分发消息msg.target.dispatchMessage(msg);
+     *      1)如果消息有callback，也就是runnable消息，直接调用run方法
+     *      2）否则如果用callback构造handler，发送消息到callback中
+     *      3）如果没有callback构造或者callback处理返回false，则发送到handleMessage中
+     * 4 handler发送消息：一般handler把消息发送到MessageQueue（handler通过Looper.myLooper()获取Looper，进而可以获得MessageQueue）
+     *
+     */
     public void run() {
        Looper.prepare();
         mHandler =new Handler(new Handler.Callback() {
@@ -38,6 +50,10 @@ public class MyThread extends Thread {
        Looper.loop();
        Log.i("ly","thread exit");
     }
+
+    /**
+     *主线程用于handler的引用，则可以在主线程中发消息到这个线程。
+     */
     public Handler getHandler(){
         return mHandler;
     }
